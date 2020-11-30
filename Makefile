1ca23a5d461c95b5c87f30b78ec83046674e6b13
Antoni Kiszka <antonikiszka@icloud.com>
2020-11-30 21:19:35 +0100

    It can run something now.

--- Makefile
+++ Makefile
@@ -1,2 +1,23 @@
-all:
-	clang++ main.cpp -std=c++17 -o main
+CC := clang++
+CFLAGS := -std=c++17 -O3
+
+OBJS = main.o
+
+.PHONY: all clean test
+
+all: containers/masno engine
+
+$(OBJS): $(OBJS:.o=.cpp)
+	$(CC) $(CFLAGS) -c $<
+
+engine: $(OBJS)
+	$(CC) $(CFLAGS) $< -o engine
+
+containers/masno: containers/masno.cpp
+	$(CC) $(CFLAGS) $< -o $@
+
+test:
+	sudo ./engine
+
+clean:
+	rm $(OBJS) engine containers/masno
--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
-#include <fstream>
-#include <iostream>
-#include <algorithm>
-#include <map>
-#include <random>
-#include <filesystem>
-#include <functional> // std::bind
+#include <fstream> // std::ifstream
+#include <iostream> // std::cout, std::cerr
+#include <algorithm> // std::remove_if
+#include <map> // std::map
+#include <random> // random ports
+#include <filesystem> // std::filesystem
 #include <stdlib.h> // itoa
 namespace fs = std::filesystem;
 
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <sched.h>
+#include <sched.h> // clone
+#include <sys/types.h> // waitpid
+#include <sys/wait.h> // waitpid, SIGCHILD
+#include <unistd.h> // execv
 
 std::map<std::string, int> configuration;
 std::string executable_name = "";
 int port = 0; // port of the containarized app
 
-int read_config() {
+int config_read() {
     std::ifstream cFile("containers.cnf");
     if (cFile.is_open()) {
         std::string line;
@@ -39,8 +38,25 @@ int read_config() {
     return 0;
 }
 
-int exec_container(void *args) {
-    printf("spawning new child: %lu\n", (unsigned long)getpid());
+
+int config_check_range() {
+    int range_begin = configuration["range_begin"];
+    int range_end = configuration["range_end"];
+    int discovery_port = configuration["discovery_port"];
+
+    if (range_begin > range_end) {
+	std::cerr << "The port range for containers is incorrect." << std::endl;
+	return -1;
+    }
+    else if (discovery_port >= range_begin && discovery_port <= range_end) {
+	std::cerr << "The discovery port must not be in the container port range." << std::endl;
+	return -1;
+    }
+    
+    return 0;
+}
+
+int container_internal_exec(void *args) {
     // unmount all
     // chroot (bind mount/pivot root dance)
     // mount /proc (make /dev?)
@@ -50,39 +66,46 @@ int exec_container(void *args) {
     char* newargv[] = { port_buffer, NULL };
     
     execv(name, newargv);
-    perror("exec");
+    std::cerr << "Starting a container failed." << std::endl;
     exit(EXIT_FAILURE);
     
     return 0;
 }
 
-pid_t run_container(std::string name, int _port) {
+pid_t container_run(std::string name, int _port) {
     char* stack = static_cast<char*>(malloc(4096)) + 4096;
     int namespaces = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWNS;// | CLONE_NEWNET
 
     executable_name = name;
     port = _port;
-    pid_t p = clone(exec_container, stack, SIGCHLD|namespaces, NULL);
+    pid_t p = clone(container_internal_exec, stack, SIGCHLD|namespaces, NULL);
 
     if (p == -1) {
-        perror("clone");
-        exit(1);
+	std::cerr << "Error while cloning the container." << std::endl;
+        exit(EXIT_FAILURE);
     }
 
+    std::cout << "Spawning a container (pid: " << p << ", executable name: \"" << executable_name << "\")." << std::endl;
+    
     return p;
 }
 
-int main(int argc, char** argv) {
-    if (read_config() != 0) return 1;
+void container_wait(pid_t pid) {
+    waitpid(pid, NULL, 0);
+}
 
+int main(int argc, char** argv) {
+    if (config_read() != 0) return EXIT_FAILURE;
+    if (config_check_range() != 0) return EXIT_FAILURE;
     // Ports for containers to run on will be generated at random
     std::default_random_engine random_engine;
     std::uniform_int_distribution<int> random_port_distribution(configuration["range_begin"], configuration["range_end"]);
     
-    std::cout << "The container engine running on ports from " << configuration["range_begin"] << " to " << configuration["range_end"] << std::endl;
+    std::cout << "The container engine is running on ports from " << configuration["range_begin"] << " to " << configuration["range_end"] << "." << std::endl;
 
-    pid_t p = run_container("./containers/masno", random_port_distribution(random_engine));
-    printf("child pid: %d\n", p);
-    waitpid(p, NULL, 0);
+    // TODO: check if the range is correct
+    
+    pid_t p = container_run("./containers/masno", random_port_distribution(random_engine));
+    container_wait(p);
     return 0;
 }
